c0b5fd41907d2bb03e8e7eac1d9f4e02ddccf6f7
GustavoMaritan <gustavo@engsolutions.com.br>
2019-09-17 13:57:37 -0300

    master - ts

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,105 @@
-import mssql = require('mssql');
+declare namespace sql {
 
-declare interface IConfig {
-    server?: string;
-    database?: string;
-    user?: string;
-    password?: string;
+    type SqlType = {
+        type: any,
+        value: any
+    }
+
+    type Config = {
+        server: string;
+        database: string;
+        user: string;
+        password: string;
+    };
+
+    interface ISqlTypes {
+        varChar(value?: String): SqlType;
+        nVarChar(value?: String): SqlType;
+        text(value?: String): SqlType;
+        int(value?: Number): SqlType;
+        bigInt(value?: Number): SqlType;
+        tinyInt(value?: Number): SqlType;
+        smallInt(value?: Number): SqlType;
+        bit(value?: Number): SqlType;
+        float(value?: Number): SqlType;
+        numeric(value?: Number): SqlType;
+        decimal(value?: Number): SqlType;
+        real(value?: Number): SqlType;
+        date(value?: Date): SqlType;
+        dateTime(value?: Date): SqlType;
+        dateTime2(value?: Date): SqlType;
+        dateTimeOffset(value?: Date): SqlType;
+        smallDateTime(value?: Date): SqlType;
+        time(value?: TimerHandler): SqlType;
+        uniqueIdentifier(value?: String): SqlType;
+        smallMoney(value?: Number): SqlType;
+        money(value?: Number): SqlType;
+        binary(value?: Number): SqlType;
+        varBinary(value?: Number): SqlType;
+        char(value?: String): SqlType;
+        nChar(value?: String): SqlType;
+        nText(value?: String): SqlType;
+    }
+
+    interface ISqlReturn {
+        content: object;
+        outputs: object;
+        returnValue: any;
+    }
+
+    interface ISqlIsolation {
+        readCommitted: String;
+        readUncommitted: String;
+        repeatable: String;
+        serializable: String;
+        snapshot: String;
+    }
+
+    interface SqlInstance {
+        isolation: ISqlIsolation;
+        types: ISqlTypes;
+        /**
+         * 
+         * @param parameters 
+         * @param model Formatar retorno de datasets
+         * @example
+         * 
+         * // Exemplo Parameters
+         * var param = { 
+         *      name: 'exemplo',                    // Input param
+         *      $return_value: sql.types.int()      // Output param
+         * };
+         * 
+         * // Exemplo Model
+         * var modelEx = { 
+         *      usuario: {}, 
+         *      telefones: [] 
+         * }; 
+         * // Resulta em { content: {usuario:{}, telefones:[]}}
+         * //------
+         */
+        execute(procedure: String, parameters?: any, model?: Object): Promise<ISqlReturn>;
+        /**
+         * 
+         * @param parameters 
+         * @example
+         * 
+         * // Exemplo Parameters
+         * var param = { 
+         *      name: 'exemplo',            // Input param
+         *      $return_value: sql.int()    // Output param
+         * };
+         */
+        executeOne(procedure: String, parameters?: any): Promise<ISqlReturn>;
+        openTransaction(isolation: ISqlIsolation): Promise<void>;
+        commit(): Promise<void>;
+        rollback(): Promise<void>;
+    }
+
+    interface Sql {
+        new(config: Config): SqlInstance;
+    }
 }
 
-declare interface IReturn {
-    server?: string;
-    database?: string;
-    user?: string;
-    password?: string;
-}
-
-declare interface IParameters {
-    server?: string;
-    database?: string;
-    user?: string;
-    password?: string;
-}
-
-export declare class Sql {
-    constructor(config: IConfig);
-    execute(procedure: string, parameters: IParameters, model: any): Promise<IReturn>;
-    executeOne(procedure: string, parameters: IParameters, model: any): Promise<IReturn>;
-    query(query: string, params: Object): Promise<IReturn>;
-}
+declare var sql: sql.Sql;
+export = sql;
